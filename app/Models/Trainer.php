ed94250be2381a5fab58b5e4fa769f8bca6f9f47
Luis anthony <luitoabol@alu.edu.gva.es>
2024-04-13 19:07:02 +0200

    Modelos Athlete y Trainer

--- app/Models/Trainer.php
+++ app/Models/Trainer.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Trainer extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function athletes()
+    {
+        return $this->hasMany(Athlete::class);
+    }
 }
