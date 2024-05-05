6daf22e7b2b4be1c1f92cea729d21c8e4befedd1
Luis anthony <luitoabol@alu.edu.gva.es>
2024-05-05 21:42:27 +0200

    Update Styles Index-Trainer

--- resources/views/trainer/index.blade.php
+++ resources/views/trainer/index.blade.php
@@ -5,7 +5,18 @@
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 @if ($athletes->isEmpty())
-                    Aún no hay atletas
+                    <div class="flex items-center p-4 text-sm text-gray-800 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600"
+                        role="alert">
+                        <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                        </svg>
+                        <span class="sr-only">Info</span>
+                        <div>
+                            <span class="font-medium">No hay atletas</span>
+                        </div>
+                    </div>
                 @else
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -38,12 +49,12 @@
                                     {{ $athlete->days_available_week }}
                                 </td>
                                 <td class="px-6 py-4">
-                                    <a href="#"
+                                    <a href="{{ route('planning')}}"
                                         class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Planificación</a>
                                 </td>
                             </tr>
                         @endforeach
-                    @endif
+                @endif
                 </tbody>
             </table>
         </div>
